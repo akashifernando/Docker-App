5a24e3e543153130f1d698f4bcdb8e89a8fab2cb
Akashi fernando <akashanifernando@gmail.com>
2026-01-31 16:01:27 +0530

    modified server .env and index.jsue

--- server/index.js
+++ server/index.js
@@ -3,6 +3,9 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const dbURI = process.env.MONGO_URI || "mongodb://localhost:27017/taskdb";
+
+console.log('Connecting to MongoDB at:', dbURI);
 
 const authRoutes = require('./src/routes/authRoutes');
 const taskRoutes = require('./src/routes/taskRoutes');
@@ -11,10 +14,10 @@ const app = express();
 app.use(express.json());
 app.use(cors()); // Allow all origins for debugging
 
-console.log('Connecting to MongoDB at:', process.env.MONGO_URI);
+console.log('Connecting to MongoDB at:', dbURI);
 
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(dbURI)
   .then(() => console.log('Mongo connected'))
   .catch((err) => console.error('Mongo connection error:', err));
 
